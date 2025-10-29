786cf4488454c6dade9d3a8440ee64a4990914fd
kelsaur <kellyb.karner@gmail.com>
2025-10-29 14:07:46 +0100

    feat: Set up React Router and global font styling

--- meetup-frontend/src/App.jsx
+++ meetup-frontend/src/App.jsx
@@ -1,31 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Routes, Route } from 'react-router-dom'
+import MeetupList from './pages/MeetupList'
 
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
-//Test
   return (
     <>
-      <div class="flex justify-center bg-red-100">
-  
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button class="bg-blue-500 hover:bg-fuchsia-500 p-1 rounded-md" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Testing Tailwind
-        </p>
-      </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        {/*<Route path="/register" element={<Register />} /> This will be used once the page is created*/}
+        {/*<Route path="/login" element={<Login />} /> This will be used once the page is created*/}
+        {/*<Route path="/dashboard" element={<MeetupList />} /> This will be used after merge with landing page branch*/}
+        {/*<Route path="/meetup" element={<MeetupDetail />} /> This will be used once the page is created*/}
+        {/*<Route path="/profile" element={<Profile />} /> This will be used once the page is created*/}
+        <Route path="/dashboard" element={<MeetupList />} />
+        
+      </Routes>
     </>
   )
 }
