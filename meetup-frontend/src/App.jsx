316e0c6a432632e33febcc510e377fdb17cf99dc
Camilla Lundahl <Camillalundahl@outlook.com>
2025-10-31 09:34:10 +0100

    feat: Implement Login page and reusable InputField component

--- meetup-frontend/src/App.jsx
+++ meetup-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import './App.css';
 import Register from './pages/Register';
+import Login from './pages/Login'; 
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register />} /> 
-        {/*<Route path="/login" element={<Login />} /> This will be used once the page is created*/}
+        <Route path="/login" element={<Login />} /> 
         {/*<Route path="/dashboard" element={<MeetupList />} /> This will be used after merge with landing page branch*/}
         {/*<Route path="/meetup" element={<MeetupDetail />} /> This will be used once the page is created*/}
         {/*<Route path="/profile" element={<Profile />} /> This will be used once the page is created*/}
